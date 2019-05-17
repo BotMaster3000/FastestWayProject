a1d98f6e9b65867abe65c58720bb13d237100448
BotMaster3000 <jdgebauer@gmx.de>
2019-05-17 23:08:54 +0200

    Added A TestCase-Scenario and started writing the first UnitTest for the NetworkParser

--- FastestWayHelperLibTests/Parsers/TrainNetworkParserTests.cs
+++ FastestWayHelperLibTests/Parsers/TrainNetworkParserTests.cs
@@ -5,15 +5,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FastestWayProject.Interfaces;
+using System.IO;
 
 namespace FastestWayProject.Parsers.Tests
 {
     [TestClass]
     public class TrainNetworkParserTests
     {
+        const string RELATIVE_FILEPATH = @"\TestFiles\TestCase2LinesCross.txt";
+
         [TestMethod]
         public void ParseTrainNetworkTest()
         {
+            string totalFilePath = Path.GetFullPath(Directory.GetCurrentDirectory() + RELATIVE_FILEPATH);
+
+            TrainNetworkParser parser = new TrainNetworkParser();
+            ITrainNetwork network = parser.ParseTrainNetwork(totalFilePath);
+
             Assert.Fail();
         }
     }
