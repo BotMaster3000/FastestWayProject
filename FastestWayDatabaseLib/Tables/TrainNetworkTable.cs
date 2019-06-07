a8db45d93f534ca4b2dd526a5b59a777224eb81d
BotMaster3000 <jdgebauer@gmx.de>
2019-06-07 21:37:53 +0200

    Added ITable-Interface and the Basic Table-Layouts

---
+++ FastestWayDatabaseLib/Tables/TrainNetworkTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastestWayDatabaseLib.Interfaces;
+
+namespace FastestWayDatabaseLib.Tables
+{
+    internal class TrainNetworkTable : ITable
+    {
+        public string TableName { get; }
+    }
+}
