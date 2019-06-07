75eb4d7e0b232628ccb387df2350f9b7d42bc679
BotMaster3000 <jdgebauer@gmx.de>
2019-06-07 22:39:18 +0200

    Added DatabaseManager-Class which will set up the Database-File and make sure all Tables are Created

---
+++ FastestWayDatabaseLib/Interfaces/IDatabaseManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastestWayDatabaseLib.Interfaces
+{
+    public interface IDatabaseManager
+    {
+        string DatabaseName { get; }
+
+        void SetupDatabase();
+    }
+}
---
+++ FastestWayDatabaseLib/Logic/DatabaseManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastestWayDatabaseLib.Interfaces;
+
+namespace FastestWayDatabaseLib.Logic
+{
+    public class DatabaseManager : IDatabaseManager
+    {
+        public string DatabaseName { get; } = "TrainNetworkDatabase.sqlite";
+
+        public void SetupDatabase()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
